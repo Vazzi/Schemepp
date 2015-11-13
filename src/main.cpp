6d07aafc8ae40015a68db9c5e737c36bc5c9b4c6
Jakub Vlasák <vlasakjakub@gmail.com>
2015-11-13 20:27:35 +0100

    Main structure

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,31 @@
-#include <cstdio>
+#include <iostream>
+
+using std::string;
+using std::cerr;
 
 int main(int argc, char *argv[]) {
-    printf("Hello this is Scheme\n");
+    string fileName;
+
+    if (argc > 1) {
+        fileName = argv[1];
+    } else {
+        cerr << "Expecting bytcode file as argument\n";
+        return 1;
+    }
+
+    try {
+        // TODO: Deserialize bytecode
+    } catch (...) {
+        cerr << "Deserialization error: " << "\n";
+        return 1;
+    }
+
+    try {
+        // TODO: Run code
+    } catch (...) {
+        cerr << "Virtual Machine error: " << "\n";
+        return 1;
+    }
+
     return 0;
 }
