f11856db94f9c2fec5e26707fc31bd7bfd6fbc02
Jakub Vlasák <vlasakjakub@gmail.com>
2015-11-15 21:15:06 +0100

    Deserialization of bytecode class

---
+++ src/Deserialization.cpp
@@ -0,0 +1,6 @@
+#include "Deserialization.hpp"
+
+// Deserialize file with byte code
+void Deserialization::deserializeByteCode(const string& fileName) {
+    // TODO: Implement
+}
---
+++ src/Deserialization.hpp
@@ -0,0 +1,14 @@
+#ifndef __DESERIALIZATION_HPP__
+#define __DESERIALIZATION_HPP__
+
+#include <string>
+
+using std::string;
+
+class Deserialization {
+public:
+    static void deserializeByteCode(const string& fileName);
+};
+
+#endif
+
