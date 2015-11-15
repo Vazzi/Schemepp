09457f4eb3021a43b16b8d86a1db4a3c9df9134e
Jakub Vlasák <vlasakjakub@gmail.com>
2015-11-15 20:47:17 +0100

    Instruction structure

--- src/ByteCode.hpp
+++ src/ByteCode.hpp
@@ -2,16 +2,25 @@
 #define __BYTECODE_HPP__
 
 // Instruction codes
-const unsigned OP_CONST = 0x00;
-const unsigned OP_LOADVAR = 0x10;
-const unsigned OP_STOREVAR = 0x11;
-const unsigned OP_DEFVAR = 0x12;
-const unsigned OP_FUNCTION = 0x20;
-const unsigned OP_POP = 0x30;
-const unsigned OP_JUMP = 0x40;
-const unsigned OP_FJUMP = 0x41;
-const unsigned OP_RETURN = 0x50;
-const unsigned OP_CALL = 0x51;
-const unsigned OP_INVALID = 0xFF;
+const unsigned int OP_CONST = 0x00;
+const unsigned int OP_LOADVAR = 0x10;
+const unsigned int OP_STOREVAR = 0x11;
+const unsigned int OP_DEFVAR = 0x12;
+const unsigned int OP_FUNCTION = 0x20;
+const unsigned int OP_POP = 0x30;
+const unsigned int OP_JUMP = 0x40;
+const unsigned int OP_FJUMP = 0x41;
+const unsigned int OP_RETURN = 0x50;
+const unsigned int OP_CALL = 0x51;
+const unsigned int OP_INVALID = 0xFF;
+
+
+// Instruction with argument
+struct Instruction {
+    Instruction(unsigned opCode = OP_INVALID, unsigned arg = 0)
+        : opCode(opCode), arg(arg) {}
+    unsigned int opCode;
+    unsigned int arg;
+}
 
 #endif
