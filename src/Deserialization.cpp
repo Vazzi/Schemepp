479886b8be59eda0bf93c45ffd232f33bf4d43cc
Jakub Vlasák <vlasakjakub@gmail.com>
2015-11-15 21:17:34 +0100

    Load file

--- src/Deserialization.cpp
+++ src/Deserialization.cpp
@@ -1,6 +1,9 @@
 #include "Deserialization.hpp"
+#include "BytecodeStream.hpp"
 
 // Deserialize file with byte code
 void Deserialization::deserializeByteCode(const string& fileName) {
+
+    BytecodeStream* stream = new BytecodeStream(fileName.c_str());
     // TODO: Implement
 }
