ad56778523c6ac5f8606e670901a0c42c156f504
Jakub Vlasák <vlasakjakub@gmail.com>
2015-11-17 21:11:54 +0100

    Next byte match type

--- src/Deserialization.cpp
+++ src/Deserialization.cpp
@@ -1,18 +1,33 @@
 #include "Deserialization.hpp"
 #include "BytecodeStream.hpp"
 #include "DeserializationError.hpp"
+#include <cstdio>
 
 const unsigned INIT_WORD = 0x00010B0B;
 
 // Deserialize file with byte code
 void Deserialization::deserializeByteCode(const string& fileName) {
 
-    BytecodeStream* stream = new BytecodeStream(fileName.c_str());
+    BytecodeStream stream = BytecodeStream(fileName.c_str());
 
-    unsigned int initWord = stream->readWord();
+    unsigned int initWord = stream.readWord();
     if (initWord != INIT_WORD) {
         throw DeserializationError("Invalid bytecode. First word is not correct.");
     }
+
+    nextByteMatchType(stream, Codeobject);
     // TODO: Implement
-    delete stream;
+}
+
+// Throw exception if next byte in stream does not match type
+void Deserialization::nextByteMatchType(BytecodeStream& stream,
+        const serializableType_t& type) {
+
+    unsigned char byte = stream.readByte();
+    if (byte != type) {
+        char *err = new char;
+        sprintf(err, "Expected type %c and got %c", type, byte);
+        string text = string(err);
+        throw DeserializationError(text);
+    }
 }
--- src/Deserialization.hpp
+++ src/Deserialization.hpp
@@ -3,11 +3,29 @@
 
 #include <string>
 
+class BytecodeStream;
+
 using std::string;
+
+enum serializableType_t {
+    Null = '0',
+    Boolean = 'b',
+    String = 's',
+    Symbol = 'S',
+    Number = 'n',
+    Pair = 'p',
+    Instr = 'i',
+    Sequence = '[',
+    Codeobject = 'c'
+};
 
 class Deserialization {
     public:
         static void deserializeByteCode(const string& fileName);
+
+    private:
+        static void nextByteMatchType(BytecodeStream& stream,
+                const serializableType_t& type);
 };
 
 #endif
