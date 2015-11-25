043b19fba348a7902b95453164d768799ed07f3f
Jakub Vlasák <vlasakjakub@gmail.com>
2015-11-25 20:03:04 +0100

    Optimized BytecodeObject

--- src/Bytecode/BytecodeObject.hpp
+++ src/Bytecode/BytecodeObject.hpp
@@ -1,23 +1,25 @@
 #ifndef __BYTECODE_OBJECT_HPP__
 #define __BYTECODE_OBJECT_HPP__
 
+#include "../Objects/BasicObject.hpp"
+#include "Bytecode.hpp"
 #include <string>
 #include <vector>
 
 using std::string;
 using std::vector;
 
-class BytecodeObject {
+class BytecodeObject : public BasicObject {
     public:
         BytecodeObject() {}
-        ~BytecodeObject() {}
+        virtual ~BytecodeObject() {}
+        string print() const { return "#BYTECODE_OBJECT#"; }
 
         string name;
         vector<string> args;
         vector<string> variableNames;
-        vector<Object*> constants;
+        vector<BasicObject*> constants;
         vector<Instruction> instructions;
-
-}
+};
 
 #endif
