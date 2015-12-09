e1f7d3177d903dd0077247c59790746e3074494d
Vojta S <shrys@seznam.cz>
2015-12-09 12:28:14 +0100

    BuiltInFunction without list

--- src/VM/BuiltIn/BuiltIn.cpp
+++ src/VM/BuiltIn/BuiltIn.cpp
@@ -2,8 +2,24 @@
 #include "BuiltInError.hpp"
 #include "Objects/SchemePair.hpp"
 #include "Objects/SchemeNumber.hpp"
+#include "Objects/SchemeBool.hpp"
+#include "Objects/SchemeSymbol.hpp"
+#include "Objects/SchemeNull.hpp"
 #include <iostream>
 #include <functional>
+#include <sstream>
+#include <typeinfo>
+#include <string>
+using std::plus;
+using std::minus;
+using std::multiplies;
+using std::divides;
+using std::equal_to;
+using std::greater;
+using std::less;
+using std::greater_equal;
+using std::less_equal;
+using std::string;
 
 using std::plus;
 using std::minus;
@@ -12,7 +28,7 @@ using std::divides;
 // TODO: Define and implement built-in functions
 // car, cdr, +, -, ...
 
-void builtInVerify(bool condition, const string& message){
+void builtInVerifyCondition(bool condition,const string& message){
     if(!condition){
         throw BuiltInError(message);
     }
@@ -28,12 +44,32 @@ T* builtInVerifyType(SchemeObject* arg, string message){
     }
     return tmpArg;
 }
+static void builtInVerifyNumberArg(BuiltInArgs& args, unsigned number, const string& name){
+    builtInVerifyCondition(args.size() == number, name + ": Wrong number of arguments"  );
+}
+
+template<class ComparisonOperation>
+static SchemeObject* builtInComparisonOperation(string name, BuiltInArgs& args, ComparisonOperation func){
+    string errorMsg = "Comparison operation " + name + " expect number";  
+    builtInVerifyCondition(args.size() > 0, "expet arguments");  
+    SchemeNumber* firstArg = builtInVerifyType<SchemeNumber>(args[0],errorMsg);    
+  
+    for (BuiltInArgs::iterator argIterator = args.begin() + 1 ; argIterator != args.end(); ++argIterator){
+        SchemeNumber* nextArgument = builtInVerifyType<SchemeNumber>(*argIterator,errorMsg);
+        if(func(firstArg->getValue(),nextArgument->getValue())){
+            firstArg = nextArgument;
+        }else{
+            return new SchemeBool(false);
+        }      
+    }
+    return new SchemeBool(true);
+}
 
 // Volba aritmeticke operace
 template<class ArithmeticOperation>
 static SchemeObject* builtInArithmeticOperation(string name, BuiltInArgs& args, ArithmeticOperation func){
     string errorMsg = "Arithmetic operation " + name + " expect number";  
-    builtInVerify(args.size() > 0, errorMsg);  
+    builtInVerifyCondition(args.size() > 0, errorMsg);  
     SchemeNumber* firstArg = builtInVerifyType<SchemeNumber>(args[0],errorMsg);    
     int result = firstArg->getValue();
     
@@ -42,7 +78,6 @@ static SchemeObject* builtInArithmeticOperation(string name, BuiltInArgs& args, 
         result = func(result, nextArgument->getValue());
     }
     return new SchemeNumber(result);
-
 }
 
 static SchemeObject* builtInPlus(BuiltInArgs& args){
@@ -57,15 +92,91 @@ static SchemeObject* builrInMul(BuiltInArgs& args){
 static SchemeObject* builrInQuot(BuiltInArgs& args){
     return builtInArithmeticOperation("/", args, divides<int>());
 }
+
 static SchemeObject* builtInCar(BuiltInArgs& args){
-    // TODO
+    builtInVerifyNumberArg(args, 1, "car");
+    string errorMsg = "car expect pair";
+    SchemePair *pair = builtInVerifyType<SchemePair>(args[0],errorMsg);
+    return pair->getFirst();
 }
 static SchemeObject* builtInCdr(BuiltInArgs& args){
-    // TODO
+    builtInVerifyNumberArg(args, 1, "cdr");
+    string errorMsg = "cdr expect pair";
+    SchemePair *pair = builtInVerifyType<SchemePair>(args[0],errorMsg);
+    return pair->getSecond();
 }
 static SchemeObject* builtInCons(BuiltInArgs& args){
-    // TODO: verifying
+    builtInVerifyNumberArg(args, 2, "cons");    
     return new SchemePair(args[0],args[1]);
+}
+
+/*------------Predicates-----------*/
+static SchemeObject* builtInIsNumber(BuiltInArgs& args){
+    builtInVerifyNumberArg(args, 1, "number?");
+    SchemeNumber* sn= dynamic_cast<SchemeNumber*>(args[0]);
+    return new SchemeBool(sn != 0);
+}
+static SchemeObject* builtInIsSymbol(BuiltInArgs& args){
+    builtInVerifyNumberArg(args, 1, "number?");
+    SchemeSymbol* ss= dynamic_cast<SchemeSymbol*>(args[0]);
+    return new SchemeBool(ss != 0);
+}
+static SchemeObject* builtInIsZero(BuiltInArgs& args){
+    builtInVerifyNumberArg(args, 1, "number?");
+    SchemeNumber* sn= dynamic_cast<SchemeNumber*>(args[0]);
+    return new SchemeBool(sn && ( sn->getValue() == 0));
+}
+static SchemeObject* builtInIsPair(BuiltInArgs& args){
+    builtInVerifyNumberArg(args, 1, "number?");
+    SchemePair* sp= dynamic_cast<SchemePair*>(args[0]);
+    return new SchemeBool(sp != 0);
+}
+static SchemeObject* builtInIsNull(BuiltInArgs& args){
+    builtInVerifyNumberArg(args, 1, "number?");
+    SchemeNull* sn= dynamic_cast<SchemeNull*>(args[0]);
+    return new SchemeBool(sn != 0);
+}
+
+static bool equalObjects(const SchemeObject *first, const SchemeObject *second){
+    if (first == second) {
+        return true;
+    } else if (typeid(*first) != typeid(*second)) {
+        return false;
+    } else {
+        return first->equalsTo(*second);
+    }
+}
+/*---same objects or same types with same values--*/
+static SchemeObject* builtInIsEqual(BuiltInArgs& args){
+    builtInVerifyNumberArg(args, 2, "eq?");
+    SchemeObject* first = args[0];
+    SchemeObject* second = args[1];
+    if(dynamic_cast<SchemePair*>(first) && dynamic_cast<SchemePair*>(second)){
+        return new SchemeBool(first == second);
+    }else{    
+        return new SchemeBool(equalObjects(first, second));
+    }
+    return builtInComparisonOperation("=", args, less_equal<int>());
+}
+
+
+/*------Comparison function----*/
+
+// compares only numbers, regardless of type.
+static SchemeObject* builtInEqTo(BuiltInArgs& args){
+    return builtInComparisonOperation("=", args, equal_to<int>());
+}
+static SchemeObject* builtInGreater(BuiltInArgs& args){
+    return builtInComparisonOperation("=", args, greater<int>());
+}
+static SchemeObject* builtInLess(BuiltInArgs& args){
+    return builtInComparisonOperation("=", args, less<int>());
+}
+static SchemeObject* builtInGreaterEq(BuiltInArgs& args){
+    return builtInComparisonOperation("=", args, greater_equal<int>());
+}
+static SchemeObject* builtInLessEq(BuiltInArgs& args){
+    return builtInComparisonOperation("=", args, less_equal<int>());
 }
 BuiltInMap mapOfAllBuiltIns() {
     BuiltInMap functions;
@@ -75,27 +186,25 @@ BuiltInMap mapOfAllBuiltIns() {
     functions["*"] = builrInMul;
     functions["/"] = builrInQuot;
     
-    /*
-    functions["car"] = builtInCar;
+    functions["car"] = builtInCar;   
     functions["cdr"] = builtInCdr;   
     functions["cons"] = builtInCons;
-   */
     
+    functions["number?"] = builtInIsNumber;
+    functions["symbol?"] = builtInIsSymbol;
+    functions["zero?"] = builtInIsZero;
+    functions["pair?"] = builtInIsPair;
+    functions["null?"] = builtInIsNull;  
+    functions["eq?"] = builtInIsEqual;
+    
+    functions["="] = builtInEqTo;
+    functions[">"] = builtInGreater;
+    functions["<"] = builtInLess;
+    functions[">="] = builtInGreaterEq;
+    functions["<="] = builtInLessEq;
+
     // TODO: Map built-in functions   
     /*        
-        functions['eq?'] = builtInEq;
-        functions['eqv?'] = builtInEqv;
-        functions['='] = builtInEqTo;
-        functions['>='] = builrInGreaterEq;
-        functions['<='] = builrInLessEq;
-        functions['>'] = builrInGreater;
-        functions['<'] = builrInLess;
-
-        functions['number?'] = builrInNumber;
-        functions['symbol?'] = builrInSymbol;
-        functions['zero?'] = builrInZero;
-        functions['pair?'] = builrInPair;
-        functions['null?'] = builrInNull;  
 
         functions['list'] = builrInList;
     */
