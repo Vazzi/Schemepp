e6daae08ab3258442d363a95aa32538039386f2d
Jakub Vlasák <vlasakjakub@gmail.com>
2015-12-14 10:58:38 +0100

    Run program skript fix

--- runProgram.sh
+++ runProgram.sh
@@ -3,8 +3,8 @@ make
 cd scheme
 python3 compile.py problem.scm
 cd ..
-cp -f scheme/problem.bobc bin/
-cp -f scheme/input.dat bin/
+cp scheme/problem.bobc bin/
+cp scheme/input.dat bin/
 cd bin/
 ./Schemepp problem.bobc -i input.dat
 
